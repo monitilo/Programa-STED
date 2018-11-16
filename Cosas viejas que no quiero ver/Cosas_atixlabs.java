a88f66867a439e75fc0230bc3c01204fd096dc35
monitilo <germanchiarelli@gmail.com>
2018-11-16 18:52:21 -0300

    Creando cosas para el coso de atixlabs

--- Cosas viejas que no quiero ver/Cosas_atixlabs.java
+++ Cosas viejas que no quiero ver/Cosas_atixlabs.java
@@ -6,6 +6,7 @@ private long numeroCuenta ;
 private String titular ;
 private long saldo ;
 private long descubiertoAcordado ;
+  
 public Cuenta ( int tipo , long nCuenta , String titular , long descAcordado ) {
 this . tipo = tipo ;
 this . numeroCuenta = nCuenta ;
@@ -14,4 +15,27 @@ if ( tipo == CTA_CORRIENTE )
 this . descubiertoAcordado = descAcordado ;
 else this . descubiertoAcordado = 0 ;
 saldo = 0 ;
+}
+
+public Cuenta ( int tipo , long numeroCuenta , String titular ) {
+this . tipo = tipo ;
+this . numeroCuenta = numeroCuenta ;
+this . titular = titular ;
+this . descubiertoAcordado = 0 ;
+saldo = 0 ;
+}
+  
+public void depositar ( long monto ) {
+saldo += monto ;
+}
+  
+public void extraer ( long monto ) throws RuntimeException {
+switch ( tipo ) {
+case CAJA_AHORRO : if ( monto > saldo )
+throw new RuntimeException ( "No hay dinero suficiente" );
+case CTA_CORRIENTE : if ( monto > saldo + descubiertoAcordado )
+throw new RuntimeException ( "No hay dinero suficiente" );
+}
+saldo -= monto ;
+}
 }
